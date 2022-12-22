fbda4f7a5d47b964e8d2540d3a58f225d6baa926
Elvan Tektaş <62278777+tektaselvan@users.noreply.github.com>
2022-12-22 23:37:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            //foreach
+            string[] arabalar = { "BMV", "Volvo", "Toyota", "Nissan", "Ford" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
